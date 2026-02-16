958dd04924e8c7f39752263d7e12bea9c9ac19f1
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2026-02-16 19:35:36 +0000

    add function to create master and slave ptys

--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <fnctl.h>
 
 static struct winsize windowSize;
 
-
+/* terminal window resizing operations */
 void handleTerminalWindowSizeChangeSignal(int signal);
 int getTerminalWindowSize(struct winsize *windowSize);
 int setTerminalWindowSize(struct winsize *windowSize);
+
+int create_terminal_session(int *master_file_descriptor, int *slave_file_descriptor);
+int fork_and_exec_shell(void);
 
 
 int main(void) {
@@ -17,7 +22,7 @@ int main(void) {
 
     //getting initial window size
     if (getTerminalWindowSize(&windowSize) == -1) return -1;
-
+:
     printf("Initial size: %d x %d px\n", windowSize.ws_xpixel, windowSize.ws_ypixel);
 
     while(1) {
@@ -45,4 +50,15 @@ int setTerminalWindowSize(struct winsize *windowSize) {
     return ioctl(STDIN_FILENO, TIOCSWINSZ, windowSize);
 }
 
+/* Create a new pseudoterminal session, this function is just a wrapper of the openpty() function
+ * the openpty() function returns the file descriptors of the master and slave pseudoterminals
+ * this function just exposes these to the UI Layer 
+ */
+int createNewPseudoterminalProcess(int *master_file_descriptor, int *slave_file_descriptor) {
+    return openpty(master_file_descriptor, slave_file_descriptor, NULL, NULL, NULL);
+}
+
+int fork_and_exec_shell(void) {
+    return 0;
+}
 
