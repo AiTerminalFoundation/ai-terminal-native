42bf9a9ef90e0cc0e6479d6f3a8e0c75841d1084
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2026-02-19 19:51:05 +0000

    duplicate file descriptors for slave pty

--- src/main.c
+++ src/main.c
@@ -61,7 +61,7 @@ int createPseudoterminalProcess(int *master_file_descriptor, int *slave_file_des
  * if we don't fork(), so we create a new child process that is the exact copy of this one,
  * then the exec() will replace the current process, and the app would crash
  */
-int fork_and_exec_shell(void) {
+int fork_and_exec_shell(int master_file_descriptor, int slave_file_descriptor) {
     // pid_t here is just an alias for an integer/long value, depending on the OS
     pid_t child_process_pid = fork();
 
@@ -72,11 +72,13 @@ int fork_and_exec_shell(void) {
         // child process, creation of the shell
         // call setsid() to start a new session, of which the child is the session leader
         // this step also causes the child to lose its controlling terminal
-
+        pid_t session_id = setsid();
 
         // use dup() to duplicate the file descriptor for the slave device on
         // STDIN STDOUT and STDERR
-        //
+        int slave_stdin_file_descriptor = dup(STDIN_FILENO);
+        int slave_stdout_file_descriptor = dup(STDOUT_FILENO);
+        int slave_stderr_file_descriptor = dup(STDERR_FILENO);
         // call exec() to start the terminal oriented program that is to be connected
         // to the pseudoterminal slave
         return 0;
