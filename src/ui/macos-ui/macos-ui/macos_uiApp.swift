fe71389dd92919f84acbfc67b826fe943f829428
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2026-02-22 13:02:15 +0000

    create swiftui project

---
+++ src/ui/macos-ui/macos-ui/macos_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_uiApp.swift
+//  macos-ui
+//
+//  Created by Michele Verriello on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct macos_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
